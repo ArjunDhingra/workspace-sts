dc58c30db470cbd11d7fd2d5796bbe791a0699fb
Arjun Dhingra <M1050760@mindtree.com>
2019-09-30 12:57:56 +0530

    First Commit

---
+++ spring-boot-microservice-currency-conversion/src/main/java/com/mindtree/springboot/microservice/example/currencyconversion/CurrencyConversionApplication.java
@@ -0,0 +1,17 @@
+package com.mindtree.springboot.microservice.example.currencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.mindtree.springboot.microservice.example.currencyconversion")
+@EnableDiscoveryClient
+public class SpringBootMicroserviceCurrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceCurrencyConversionApplication.class, args);
+	}
+
+}
