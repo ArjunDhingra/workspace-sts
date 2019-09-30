02c2ef92406d7b88d572597cec9603e3d8fc172a
Arjun Dhingra <M1050760@mindtree.com>
2019-09-30 12:51:46 +0530

    First Commit

---
+++ Microservice-ConfigService/src/main/java/org/equifax/boot/camp/SecurityConfig.java
@@ -0,0 +1,13 @@
+package org.equifax.boot.camp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/actuator/**").permitAll().anyRequest().authenticated().and().httpBasic();
+	}
+}
